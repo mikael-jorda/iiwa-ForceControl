5f292486a3fb3a84305f18c60995c4738acc2033
Mikael Jorda <mikael.jorda@gmail.com>
2017-04-13 12:21:49 -0700

    added orientation task to have a lighter controller file

---
+++ src/tasks/OrientationTask.h
@@ -0,0 +1,95 @@
+/*
+ * orientationTask.h
+ *
+ *      Author: Mikael Jorda
+ */
+
+#ifndef IIWA_FORCE_CONTROL_ORIENTATION_TASK_H_
+#define IIWA_FORCE_CONTROL_ORIENTATION_TASK_H_
+
+#include <Eigen/Dense>
+#include <string>
+
+class OrientationTask
+{
+public:
+
+	OrientationTask(int dof)
+	{
+		link_name = "no link defined";
+
+		current_orientation = Eigen::Matrix3d::Identity(3,3);
+		desired_orientation = Eigen::Matrix3d::Identity(3,3);
+
+		current_angular_velocity = Eigen::Vector3d::Zero(3);
+		desired_angular_velocity = Eigen::Vector3d::Zero(3);
+
+		kp = 0.0;
+		kv = 0.0;
+
+		jacobian = Eigen::MatrixXd::Zero(3,dof);
+		projected_jacobian = Eigen::MatrixXd::Zero(3,dof);
+		Lambda = Eigen::MatrixXd::Zero(3,3);
+		Jbar = Eigen::MatrixXd::Zero(dof,3);
+		N = Eigen::MatrixXd::Zero(dof,dof);
+
+		task_force = Eigen::Vector3d::Zero();
+		orientation_error = Eigen::Vector3d::Zero();
+	}
+
+	void orientationError(Eigen::Vector3d& delta_phi,
+		const Eigen::Matrix3d& desired_orientation,
+		const Eigen::Matrix3d& current_orientation)
+	{
+	// check that the matrices are valid rotations
+		Eigen::Matrix3d Q1 = desired_orientation*desired_orientation.transpose() - Eigen::Matrix3d::Identity();
+		Eigen::Matrix3d Q2 = current_orientation*current_orientation.transpose() - Eigen::Matrix3d::Identity();
+		if(Q1.norm() > 0.0001 || Q2.norm() > 0.0001)
+		{
+			throw std::invalid_argument("Invalid rotation matrices in ModelInterface::orientationError");
+			return;
+		}
+		else
+		{
+			Eigen::Vector3d rc1 = current_orientation.block<3,1>(0,0);
+			Eigen::Vector3d rc2 = current_orientation.block<3,1>(0,1);
+			Eigen::Vector3d rc3 = current_orientation.block<3,1>(0,2);
+			Eigen::Vector3d rd1 = desired_orientation.block<3,1>(0,0);
+			Eigen::Vector3d rd2 = desired_orientation.block<3,1>(0,1);
+			Eigen::Vector3d rd3 = desired_orientation.block<3,1>(0,2);
+			delta_phi = -1.0/2.0*(rc1.cross(rd1) + rc2.cross(rd2) + rc3.cross(rd3));
+		}
+	}
+
+	// computes the task torques. PD controller for now
+	void computeTorques(Eigen::VectorXd& task_joint_torques)
+	{
+		orientationError(orientation_error, desired_orientation, current_orientation);
+		task_force = Lambda*(-kp*orientation_error - kv*(current_angular_velocity - desired_angular_velocity ));
+
+		task_joint_torques = projected_jacobian.transpose()*task_force;
+	}
+
+	std::string link_name;
+
+	Eigen::Matrix3d current_orientation;
+	Eigen::Matrix3d desired_orientation;
+
+	Eigen::Vector3d current_angular_velocity;
+	Eigen::Vector3d desired_angular_velocity;
+
+	double kp;
+	double kv;
+
+	Eigen::MatrixXd jacobian;
+	Eigen::MatrixXd projected_jacobian;
+	Eigen::MatrixXd Lambda;
+	Eigen::MatrixXd Jbar;
+	Eigen::MatrixXd N;
+
+private:
+	Eigen::Vector3d task_force;
+	Eigen::Vector3d orientation_error;
+};
+
+#endif
