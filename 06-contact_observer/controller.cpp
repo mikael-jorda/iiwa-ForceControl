9b497cdd7eef1f996d8710ca8719fa2ce211abb6
Mikael Jorda <mjorda@stanford.edu>
2018-02-27 12:12:16 -0800

    prepared setup for collision detection on kuka

---
+++ 06-contact_observer/controller.cpp
@@ -0,0 +1,224 @@
+// This example application runs a controller for the IIWA
+
+#include "Sai2Model.h"
+#include "redis/RedisClient.h"
+#include "timer/LoopTimer.h"
+
+#include <iostream>
+#include <string>
+
+// #include "tasks/OrientationTask.h"
+// #include "tasks/HybridPositionTask.h"
+// #include "tasks/TestTask.h"
+#include "tasks/PosOriTask.h"
+
+#include <signal.h>
+bool runloop = true;
+void stop(int){runloop = false;}
+
+#define INITIAL                0
+#define GC_STOP                1
+
+using namespace std;
+int state;
+
+const string world_file = "../resources/06-contact_observer/world.urdf";
+const string robot_file = "../resources/06-contact_observer/kuka_iiwa.urdf";
+const std::string robot_name = "Kuka-IIWA";
+
+unsigned long long controller_counter = 0;
+
+const bool simulation = true;
+// const bool simulation = false;
+
+// redis keys:
+// - write:
+std::string JOINT_TORQUES_COMMANDED_KEY;
+// - read:
+std::string JOINT_ANGLES_KEY;
+std::string JOINT_VELOCITIES_KEY;
+std::string EE_FORCE_SENSOR_KEY;
+const std::string MASSMATRIX_KEY = "sai2::KUKA_IIWA::sensors::massmatrix";
+
+// - data log
+const std::string EE_DESIRED_FORCE_LOGGED_KEY = "sai2::iiwaForceControl::iiwaBot::simulation::data_log::desired_force";
+const std::string EE_SENSED_FORCE_LOGGED_KEY = "sai2::iiwaForceControl::iiwaBot::simulation::data_log::sensed_force";
+const std::string RC_KEY = "sai2::iiwaForceControl::iiwaBot::simulation::data_log::Rc";
+
+void sighandler(int sig)
+{ runloop = false; }
+
+int main() {
+	if(simulation)
+	{
+		JOINT_TORQUES_COMMANDED_KEY = "sai2::iiwaForceControl::iiwaBot::actuators::fgc";
+		JOINT_ANGLES_KEY  = "sai2::iiwaForceControl::iiwaBot::sensors::q";
+		JOINT_VELOCITIES_KEY = "sai2::iiwaForceControl::iiwaBot::sensors::dq";
+		EE_FORCE_SENSOR_KEY = "sai2::optoforceSensor::6Dsensor::force_moment";
+	}
+	else
+	{
+		JOINT_TORQUES_COMMANDED_KEY = "sai2::KUKA_IIWA::actuators::fgc";
+		JOINT_ANGLES_KEY  = "sai2::KUKA_IIWA::sensors::q";
+		JOINT_VELOCITIES_KEY = "sai2::KUKA_IIWA::sensors::dq";
+		EE_FORCE_SENSOR_KEY = "sai2::optoforceSensor::6Dsensor::force";
+	}
+
+	std::cout << "Loading URDF world model file: " << world_file << std::endl;
+
+	// start redis client
+	HiredisServerInfo info;
+	info.hostname_ = "127.0.0.1";
+	info.port_ = 6379;
+	info.timeout_ = { 1, 500000 }; // 1.5 seconds
+	auto redis_client = CDatabaseRedisClient();
+	redis_client.serverIs(info);
+
+	// set up signal handler
+	signal(SIGABRT, &sighandler);
+	signal(SIGTERM, &sighandler);
+	signal(SIGINT, &sighandler);
+
+	// load robots
+	auto robot = new Sai2Model::Sai2Model(robot_file, false);
+
+	// read from Redis
+	redis_client.getEigenMatrixDerived(JOINT_ANGLES_KEY, robot->_q);
+	redis_client.getEigenMatrixDerived(JOINT_VELOCITIES_KEY, robot->_dq);
+
+	////////////////////////////////////////////////
+	///    Prepare the different controllers   /////
+	////////////////////////////////////////////////
+	robot->updateModel();
+
+	int dof = robot->dof();
+	Eigen::VectorXd command_torques = Eigen::VectorXd::Zero(dof);
+	Eigen::MatrixXd N_prec;
+
+	// Joint control
+	Eigen::VectorXd joint_task_desired_position(dof), joint_task_torques(dof);
+	Eigen::VectorXd anti_grav_comp(dof), gravity_vector(dof);
+
+	double joint_kv = 5.0;
+
+	// position and orientation task
+	const string control_link = "link6";
+	Eigen::Affine3d control_frame = Eigen::Affine3d::Identity();
+	control_frame.translation() = Eigen::Vector3d(0.0, 0.0, 0.12);
+	Eigen::Affine3d sensor_frame = Eigen::Affine3d::Identity();
+	sensor_frame.translation() = Eigen::Vector3d(0.0, 0.0, 0.02);
+
+	auto posori_task = new PosOriTask(robot, "link6", control_frame, sensor_frame);
+
+	posori_task->_kp_ori = 600.0;
+	// posori_task->_kp_ori = 0.0;
+	posori_task->_kv_ori = 50.0;
+	posori_task->_kp_pos = 150.0;
+	// posori_task->_kp_pos = 0.0;
+	posori_task->_kv_pos = 22.0;
+
+	Eigen::VectorXd posori_task_torques(dof);
+
+	// create a loop timer
+	double control_freq = 1000;
+	LoopTimer timer;
+	timer.setLoopFrequency(control_freq);   // 1 KHz
+	// timer.setThreadHighPriority();  // make timing more accurate. requires running executable as sudo.
+	timer.setCtrlCHandler(stop);    // exit while loop on ctrl-c
+	timer.initializeTimer(1000000); // 1 ms pause before starting loop
+
+	Eigen::VectorXd sensed_force_moment = Eigen::VectorXd::Zero(6);
+	Eigen::VectorXd sensor_bias = Eigen::VectorXd::Zero(6);
+
+	state = INITIAL;
+
+	// while window is open:
+	while (runloop) {
+
+		// wait for next scheduled loop
+		timer.waitForNextLoop();
+
+		// read from Redis
+		redis_client.getEigenMatrixDerived(JOINT_ANGLES_KEY, robot->_q);
+		redis_client.getEigenMatrixDerived(JOINT_VELOCITIES_KEY, robot->_dq);
+		redis_client.getEigenMatrixDerived(EE_FORCE_SENSOR_KEY, sensed_force_moment);
+
+		// update the model 20 times slower
+		if(controller_counter%20 == 0)
+		{
+			robot->updateKinematics();
+			if(!simulation)
+			{
+				redis_client.getEigenMatrixDerived(MASSMATRIX_KEY, robot->_M);
+				robot->_M_inv = robot->_M.inverse();
+			}
+			else
+			{
+				robot->updateDynamics();
+			}
+
+			// ----------- tasks
+			N_prec = Eigen::MatrixXd::Identity(dof,dof);
+
+			posori_task->updateTaskModel(N_prec);
+			N_prec = posori_task->_N;
+		}
+
+		////////////////////////////// Compute joint torques
+		double time = controller_counter/control_freq;
+
+		//---- orientation controller
+		// update current orientation
+		robot->rotation(posori_task->_current_orientation, posori_task->_link_name);
+		robot->angularVelocity(posori_task->_current_angular_velocity, posori_task->_link_name);
+
+		//---- position controller 
+		// update current position and velocity
+		robot->position(posori_task->_current_position, posori_task->_link_name, posori_task->_T_link_control.translation());
+		robot->linearVelocity(posori_task->_current_linear_velocity, posori_task->_link_name, posori_task->_T_link_control.translation());
+
+		if(state == INITIAL)
+		{
+			posori_task->_desired_position += Eigen::Vector3d(0.1,0.0,0.0);
+			state = GC_STOP;
+			cout << "stop nulspace gavity compensation\n" << endl;
+		}
+
+		if(state == GC_STOP)
+		{
+			robot->gravityVector(gravity_vector);
+			anti_grav_comp = N_prec.transpose() * gravity_vector;
+			// cout << gravity_vector.transpose() << endl;
+		}
+
+		// compute joint torques
+		posori_task->computeTorques(posori_task_torques);
+
+		//----- Joint nullspace damping
+		joint_task_torques = robot->_M*( - joint_kv*robot->_dq);
+
+		//------ Final torques
+		command_torques = posori_task_torques + N_prec.transpose()*joint_task_torques - anti_grav_comp;
+
+		redis_client.setEigenMatrixDerived(JOINT_TORQUES_COMMANDED_KEY, command_torques);
+
+		redis_client.setEigenMatrixDerived(EE_DESIRED_FORCE_LOGGED_KEY, posori_task->_desired_force);
+		redis_client.setEigenMatrixDerived(EE_SENSED_FORCE_LOGGED_KEY, sensed_force_moment.head(3));
+		redis_client.setCommandIs(RC_KEY,std::to_string(posori_task->_Rc_force));
+
+		controller_counter++;
+
+	}
+
+    command_torques << 0,0,0,0,0,0,0;
+    redis_client.setEigenMatrixDerived(JOINT_TORQUES_COMMANDED_KEY, command_torques);
+
+    double end_time = timer.elapsedTime();
+    std::cout << "\n";
+    std::cout << "Loop run time  : " << end_time << " seconds\n";
+    std::cout << "Loop updates   : " << timer.elapsedCycles() << "\n";
+    std::cout << "Loop frequency : " << timer.elapsedCycles()/end_time << "Hz\n";
+
+
+    return 0;
+}
---
+++ src/tasks/PosOriTask.h
@@ -0,0 +1,316 @@
+/*
+ * PosOriTask.h
+ *
+ *      This class creates a 6Dof position + orientation hybrid controller for a robotic manipulator using operational space formulation and an underlying PID compensator.
+ *      If used for hybrid position force control, assumes a force sensor is attached to the same link as the control frame and the force sensed values are given in sensor frame.
+ *      Besides, the force sensed and moment sensed are assumed to be the force and moment that the robot applies to the environment.
+ *      It requires a robot model parsed from a urdf file to a Sai2Model object, as well as the definition of a control frame
+ *      as a link at which the frame is attached, and an affine transform that determines the position and orientation of the control frame in this link
+ *
+ *      Author: Mikael Jorda
+ */
+
+#ifndef IIWAFORCECONTROL_POSORI_TASK_H_
+#define IIWAFORCECONTROL_POSORI_TASK_H_
+
+#include "Sai2Model.h"
+#include <Eigen/Dense>
+#include <string>
+#include <chrono>
+
+class PosOriTask
+{
+public:
+
+	//------------------------------------------------
+	// Constructors
+	//------------------------------------------------
+
+	/**
+	 * @brief Constructor that takes an Affine3d matrix for definition of the control frame. Creates a full position controller by default.
+	 * 
+	 * @param robot           A pointer to a Sai2Model object for the robot that is to be controlled	
+	 * @param link_name       The name of the link in the robot at which to attach the control frame
+	 * @param control_frame The position and orientation of the control frame in local link coordinates
+	 * @param sensor_frame  The position and orientation of the sensor frame in local link coordinates
+	 */
+	PosOriTask(Sai2Model::Sai2Model* robot, 
+		            const std::string link_name, 
+		            const Eigen::Affine3d control_frame = Eigen::Affine3d::Identity(),
+		            const Eigen::Affine3d sensor_frame = Eigen::Affine3d::Identity());
+
+	//------------------------------------------------
+	// Methods
+	//------------------------------------------------
+
+	// -------- core methods --------
+
+	/**
+	 * @brief update the task model (jacobians, task inertia and nullspace matrices)
+	 * @details This function updates the jacobian, projected jacobian, task inertia matrix (Lambda), dynamically consistent inverse of the Jacobian (Jbar)
+	 * and nullspace matrix of the task N. This function uses the robot model and assumes it has been updated. 
+	 * There is no use to calling it if the robot kinematics or dynamics have not been updated since the last call.
+	 * This function takes the N_prec matrix as a parameter which is the product of the nullspace matrices of the higher priority tasks.
+	 * The N matrix will be the matrix to use as N_prec for the subsequent tasks.
+	 * In order to get the nullspace matrix of this task alone, one needs to compute _N * _N_prec.transpose().	
+	 * 
+	 * @param N_prec The nullspace matrix of all the higher priority tasks. If this is the highest priority task, use identity of size n*n where n in the number of DoF of the robot.
+	 */
+	void updateTaskModel(const Eigen::MatrixXd N_prec);
+
+	/**
+	 * @brief Computes the torques associated with this task.
+	 * @details Computes the torques taking into account the last model update and updated values for the robot joint positions/velocities
+	 * assumes the desired orientation and angular velocity has been updated
+	 * 
+	 * @param task_joint_torques the vector to be filled with the new joint torques to apply for the task
+	 */
+	void computeTorques(Eigen::VectorXd& task_joint_torques);
+
+	// -------- force control related methods --------
+
+	void setForceSensorFrame(const std::string link_name, const Eigen::Affine3d sensor_frame);
+
+	/**
+	 * @brief Updates the velues of the sensed force and sensed moment from sensor values
+	 * @details Assumes that the sensor is attached to the same link as the control frame and that the member variable _T_control_sensor has been set as the 
+	 * transformation matrix from the control frame to the sensor frame with the function setSensorFrame.
+	 * The force and moment values given to this function are assumed to be in the force sensor frame (values taken directly from the force sensor)
+	 * These values are supposed to be the forces that the sensor applies to the environment (so the opposite of what the sensor feels)
+	 * 
+	 * @param sensed_force_sensor_frame The sensed force as the force that the sensor applies to the environment in sensor frame
+	 * @param sensed_moment_sensor_frame The sensed moment as the moment that the sensor applies to the environment in sensor frame
+	 */
+	void updateSensedForceAndMoment(const Eigen::Vector3d sensed_force_sensor_frame, 
+								    const Eigen::Vector3d sensed_moment_sensor_frame);
+
+	/**
+	 * @brief Sets the force controlled axis for a hybrid position force controller with 1 DoF force and 2 DoF motion
+	 * @details This is the function to use in order to get the position part of the controller behave as a Hybrid Force/Motion controller with 1 Dof force.
+	 * The motion is controlled orthogonally to the force.
+	 * It can be called anytime to change the behavior of the controller and reset the integral terms.
+	 * 
+	 * @param force_axis         The axis in control frame coordinates along which the controller behaves as a force controller.
+	 */
+	void setForceAxis(const Eigen::Vector3d force_axis);
+
+	/**
+	 * @brief Updates the force controlled axis for a hybrid position force controller with 1 DoF force and 2 DoF motion
+	 * @details Use this function in situations when the force axis needs to be updated (as an estimated normal for example) over time.
+	 * This does not reset the integral terms.
+	 * In setting up the controller for the first time, prefer setForceAxis.
+	 * 
+	 * @param force_axis         The axis in control frame coordinates along which the controller behaves as a force controller.
+	 */
+	void updateForceAxis(const Eigen::Vector3d force_axis);
+
+	/**
+	 * @brief Sets the motion controlled axis for a hybrid position force controller with 2 DoF force and 1 DoF motion
+	 * @details This is the function to use in order to get the position part of the controller behave as a Hybrid Force/Motion controller with 2 Dof force.
+	 * The motion is controlled along one axis and the force is controlled orthogonally to the motion
+	 * It can be called anytime to change the behavior of the controller and reset the integral terms.
+	 * 
+	 * @param force_axis         The axis in control frame coordinates along which the controller behaves as a motion controller.
+	 */
+	void setLinearMotionAxis(const Eigen::Vector3d motion_axis);
+
+	/**
+	 * @brief Sets the motion controlled axis for a hybrid position force controller with 2 DoF force and 1 DoF motion
+	 * @details Use this function in situations when the motion axis needs to be updated over time.
+	 * This does not reset the integral terms.
+	 * In setting up the controller for the first time, prefer setMotionAxis.
+	 * 
+	 * @param force_axis         The axis in control frame coordinates along which the controller behaves as a motion controller.
+	 */
+	void updateLinearMotionAxis(const Eigen::Vector3d motion_axis);
+
+	/**
+	 * @brief Sets the linear part of the task as a full 3DoF force controller
+	 * @details This is the function to use in order to get the position part of the controller behave as pure Force controller with 3 Dof force.
+	 * It can be called anytime to change the behavior of the controller and reset the integral terms.
+	 */
+	void setFullForceControl();
+
+	/**
+	 * @brief Sets the linear part of the task as a full 3DoF motion controller
+	 * @details This is the function to use in order to get the position part of the controller behave as pure Motion controller with 3 Dof linear motion.
+	 * It is de default behavior of the controller.
+	 * It can be called anytime to change the behavior of the controller and reset the integral terms.
+	 */
+	void setFullLinearMotionControl();
+
+	/**
+	 * @brief Sets the moment controlled axis for a hybrid orientation moment controller with 1 DoF moment and 2 DoF orientation
+	 * @details This is the function to use in order to get the orientation part of the controller behave as a Hybrid Force/Motion controller with 1 Dof moment.
+	 * The rotational motion is controlled orthogonally to the moment.
+	 * It can be called anytime to change the behavior of the controller and reset the integral terms.
+	 * 
+	 * @param moment_axis         The axis in control frame coordinates along which the controller behaves as a moment controller.
+	 */
+	void setMomentAxis(const Eigen::Vector3d moment_axis);
+
+	/**
+	 * @brief Sets the moment controlled axis for a hybrid orientation moment controller with 1 DoF moment and 2 DoF orientation
+	 * @details Use this function in situations when the moment axis needs to be updated over time.
+	 * This does not reset the integral terms.
+	 * In setting up the controller for the first time, prefer setMomentAxis.
+	 * 
+	 * @param moment_axis         The axis in control frame coordinates along which the controller behaves as a moment controller.
+	 */
+	void updateMomentAxis(const Eigen::Vector3d moment_axis);
+
+	/**
+	 * @brief Sets the angular movement controlled axis for a hybrid orientation moment controller with 2 DoF moment and 1 DoF motion
+	 * @details This is the function to use in order to get the orientation part of the controller behave as a Hybrid Force/Motion controller with 2 Dof moment.
+	 * The rotational motion is controlled along one axis and the moment is controlled orthogonally to the motion
+	 * It can be called anytime to change the behavior of the controller and reset the integral terms.
+	 * 
+	 * @param force_axis         The axis in control frame coordinates along which the controller behaves as a rotational motion controller.
+	 */
+	void setAngularMotionAxis(const Eigen::Vector3d motion_axis);
+
+	/**
+	 * @brief Sets the angular movement controlled axis for a hybrid orientation moment controller with 2 DoF moment and 1 DoF motion
+	 * @details Use this function in situations when the angular motion axis needs to be updated over time.
+	 * This does not reset the integral terms.
+	 * In setting up the controller for the first time, prefer setAngularMotionAxis.
+	 * 
+	 * @param force_axis         The axis in control frame coordinates along which the controller behaves as a rotational motion controller.
+	 */
+	void updateAngularMotionAxis(const Eigen::Vector3d motion_axis);
+
+	/**
+	 * @brief Sets the angular part of the task as a full 3DoF moment controller
+	 * @details This is the function to use in order to get the orientation part of the controller behave as pure moment controller with 3 Dof moment.
+	 * It can be called anytime to change the behavior of the controller and reset the integral terms.
+	 */
+	void setFullMomentControl();
+
+	/**
+	 * @brief Sets the angular part of the task as a full 3DoF motion controller
+	 * @details This is the function to use in order to get the orientation part of the controller behave as pure Motion controller with 3 Dof angular motion.
+	 * It is de default behavior of the controller.
+	 * It can be called anytime to change the behavior of the controller and reset the integral terms.
+	 */
+	void setFullAngularMotionControl();
+
+	/**
+	 * @brief Changes the behavior to closed loop force control for the force controlled directions in the linear part of the controller
+	 */
+	void setClosedLoopForceControl();
+
+	/**
+	 * @brief Changes the behavior to open loop force control for the force controlled directions in the linear part of the controller
+	 * (default behavior)
+	 */
+	void setOpenLoopForceControl();
+
+	/**
+	 * @brief Changes the behavior to closed loop moment control for the moment controlled directions in the angular part of the controller
+	 */
+	void setClosedLoopMomentControl();
+
+	/**
+	 * @brief Changes the behavior to open loop moment control for the moment controlled directions in the angular part of the controller
+	 */
+	void setOpenLoopMomentControl();
+	// ------- helper methods -------
+
+	/**
+	 * @brief Resets all the integrated errors used in I terms
+	 */
+	void resetIntegrators();
+
+	/**
+	 * @brief Resets the integrated errors used in I terms for linear part of task (position_integrated_error and force_integrated_error)
+	 */
+	void resetIntegratorsLinear();
+
+	/**
+	 * @brief Resets the integrated errors used in I terms for angular part of task (orientation_integrated_error and moment_integrated_error)
+	 */
+	void resetIntegratorsAngular();
+
+	//------------------------------------------------
+	// Attributes
+	//------------------------------------------------
+
+	Sai2Model::Sai2Model* _robot;
+	std::string _link_name;
+	Eigen::Affine3d _T_link_control;   // in link_frame
+
+	// movement quantities
+	Eigen::Vector3d _current_position;      // robot frame
+	Eigen::Vector3d _desired_position;      // robot frame
+	Eigen::Matrix3d _current_orientation;   // robot frame
+	Eigen::Matrix3d _desired_orientation;   // robot frame
+
+	Eigen::Vector3d _current_linear_velocity;           // robot frame
+	Eigen::Vector3d _desired_linear_velocity;           // robot frame
+	Eigen::Vector3d _current_angular_velocity;   // robot frame
+	Eigen::Vector3d _desired_angular_velocity;   // robot frame
+
+	double _kp_pos, _kp_ori;
+	double _kv_pos, _kv_ori;
+	double _ki_pos, _ki_ori;
+
+	Eigen::Vector3d _orientation_error;               // robot frame
+	Eigen::Vector3d _integrated_orientation_error;    // robot frame
+	Eigen::Vector3d _integrated_position_error;       // robot frame
+	
+	Eigen::Matrix3d _sigma_position;        // control frame
+	Eigen::Matrix3d _sigma_orientation;     // control frame
+
+	// force quantities
+	Eigen::Affine3d _T_link_sensor; 
+	Eigen::Affine3d _T_control_sensor;
+
+	Eigen::Vector3d _desired_force;   // robot frame
+	Eigen::Vector3d _sensed_force;    // robot frame
+	Eigen::Vector3d _desired_moment;  // robot frame
+	Eigen::Vector3d _sensed_moment;   // robot frame
+
+	double _kp_force, _kp_moment;
+	double _kv_force, _kv_moment;
+	double _ki_force, _ki_moment;
+
+	Eigen::Vector3d _integrated_force_error;    // robot frame
+	Eigen::Vector3d _integrated_moment_error;   // robot frame
+
+	Eigen::Matrix3d _sigma_force;     // control frame
+	Eigen::Matrix3d _sigma_moment;    // control frame
+
+	bool _closed_loop_force_control;
+	bool _closed_loop_moment_control;
+
+	// passivity related things
+	bool _passivity_enabled;
+	double _Rc_force;
+	double _Rc_moment;
+	double _PO_force;
+	double _PO_moment;
+
+	Eigen::VectorXd _PO_buffer_force;
+	Eigen::VectorXd _PO_buffer_moment;
+
+	// task force (6D vector of forces and moments at control frame)
+	Eigen::VectorXd _task_force;   // robot frame
+
+	// model quantities
+	Eigen::MatrixXd _jacobian;
+	Eigen::MatrixXd _projected_jacobian;
+	Eigen::MatrixXd _Lambda;
+	Eigen::MatrixXd _Jbar;
+	Eigen::MatrixXd _N;
+	Eigen::MatrixXd _N_prec;
+
+	// timing for I term
+	std::chrono::high_resolution_clock::time_point _t_prev;
+	std::chrono::high_resolution_clock::time_point _t_curr;
+	std::chrono::duration<double> _t_diff;
+	bool _first_iteration;
+};
+
+
+/* IIWAFORCECONTROL_POSORI_TASK_H_ */
+#endif
